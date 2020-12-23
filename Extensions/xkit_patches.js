677932e491b10eb32b2ddf47da67f48e9f84748b
motackt <42635772+motackt@users.noreply.github.com>
2020-12-23 22:35:44 +1100

    Revert "Debounce post listener (#1977)"
    This reverts commit 3f02980ca6d668defec6e1acdbc27dd969faf3da.

--- Extensions/xkit_patches.js
+++ Extensions/xkit_patches.js
@@ -1,5 +1,5 @@
 //* TITLE XKit Patches **//
-//* VERSION 7.4.10 **//
+//* VERSION 7.4.9 **//
 //* DESCRIPTION Patches framework **//
 //* DEVELOPER new-xkit **//
 
@@ -206,16 +206,12 @@ XKit.extensions.xkit_patches = new Object({
 				}
 			};
 
-			XKit.post_listener.debounce_timer = null;
-
 			XKit.post_listener.observer = new MutationObserver(mutations => {
 				const criteria = XKit.page.react ? "[data-id]" : ".post_container, .post";
-				let new_posts = false;
-				const observed = mutations.some(({addedNodes, target}) => {
+				const new_posts = mutations.some(({addedNodes, target}) => {
 					for (let i = 0; i < addedNodes.length; i++) {
 						const $addedNode = $(addedNodes[i]);
 						if ($addedNode.is(criteria) || $addedNode.find(criteria).length) {
-							new_posts = true;
 							return true;
 						}
 					}
@@ -223,26 +219,16 @@ XKit.extensions.xkit_patches = new Object({
 					return $(target).parents(criteria).length !== 0;
 				});
 
-				if (observed) {
-					const self = XKit.post_listener;
-					clearTimeout(self.debounce_timer);
-					if (new_posts) {
-						self.run_callbacks();
-					} else {
-						self.debounce_timer = setTimeout(self.run_callbacks, 60);
-					}
+				if (new_posts) {
+					Object.values(XKit.post_listener.callbacks).forEach(list => list.forEach(callback => {
+						try {
+							callback();
+						} catch (e) {
+							console.error(e);
+						}
+					}));
 				}
 			});
-
-			XKit.post_listener.run_callbacks = function() {
-				Object.values(XKit.post_listener.callbacks).forEach(list => list.forEach(callback => {
-					try {
-						callback();
-					} catch (e) {
-						console.error(e);
-					}
-				}));
-			};
 
 			/**
 			 * Show an XKit alert window
