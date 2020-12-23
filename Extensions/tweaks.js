6cbb12cdaed0bc1cb25ad584761a7ab5043ccaf6
motackt <42635772+motackt@users.noreply.github.com>
2020-12-23 22:35:44 +1100

    Revert "Older and redder notification badges (#1896)"
    This reverts commit 7e23ec02416318a403b02de56d5fed237052bb3d.

--- Extensions/tweaks.js
+++ Extensions/tweaks.js
@@ -1,5 +1,5 @@
 //* TITLE Tweaks **//
-//* VERSION 6.0.8 **/
+//* VERSION 6.0.7 **/
 //* DESCRIPTION Various little tweaks for your dashboard. **//
 //* DEVELOPER new-xkit **//
 //* DETAILS These are small little tweaks that allows you customize your dashboard. If you have used XKit 6, you will notice that some of the extensions have been moved here as options you can toggle. Keep in mind that some of the tweaks (the ones marked with a '*') can slow down your computer. **//
@@ -307,8 +307,7 @@ XKit.extensions.tweaks = new Object({
 				"Default", "default",
 				"Hidden", "hidden",
 				"White", "white",
-				"Old Red (Palette-dependent)", "oldred",
-				"Old Red (Always red)", "olderredder",
+				"Old Red", "oldred",
 			]
 		},
 		hide_activity_notification_badge: {
@@ -795,10 +794,6 @@ XKit.extensions.tweaks = new Object({
 				case "oldred":
 					notificationBadgeStyle = `background: var(--red) !important;
 						color: var(--white-on-dark) !important;`;
-					break;
-				case "olderredder":
-					notificationBadgeStyle = `background: #bb2502 !important;
-						color: #fff !important;`;
 					break;
 			}
 			XKit.extensions.tweaks.add_css(`${notificationBadgeSel} {
